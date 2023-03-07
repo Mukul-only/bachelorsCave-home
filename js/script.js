46132fb2991e5fb1c3545907709c41817a6a3b71
unknown <mukul.kushwahaa@gmail.com>
2023-03-07 15:08:31 +0530

    v1bets

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const radioDiv = document.querySelectorAll('.drd');
+const radio = document.querySelectorAll('.rd');
+const menuBtn = document.querySelector('#menu-btn');
+const menu = document.querySelector("#menu");
+
+
+menuBtn.addEventListener('click',()=>{
+    menuBtn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+    document.querySelector('body').classList.toggle('overflow-hidden');
+});
+
+
+
+for(let i = 0 ; i<radioDiv.length ; i++){
+    radioDiv[i].addEventListener('click',()=>{
+        if(radio[i].checked){
+           radioDiv[i].classList.add('selected');
+           for(let j=0 ; j<radioDiv.length;j++){
+                if(j != i){
+
+                    radioDiv[j].classList.remove('selected');
+                }
+           }
+        }
+       });
+       
+}
+
